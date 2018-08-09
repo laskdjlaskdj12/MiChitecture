971af05762f8394dcb2e0048cc48fab27b250ec7
InHoeKu <laskdjlaskdj12@gmail.com>
2018-08-09 21:08:09 +0900

    deprecated 추가

--- src/main/java/com/laskdjlaskdj12/Player/PlayerBlockStorageCache.java
+++ src/main/java/com/laskdjlaskdj12/Player/PlayerBlockStorageCache.java
@@ -1,13 +1,16 @@
 package com.laskdjlaskdj12.Player;
 
 import com.laskdjlaskdj12.ScanBlockStruct.ScanBlockStruct;
+import com.laskdjlaskdj12.VO.PlayerBlockAreaInfoVO;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Vector;
 
 public class PlayerBlockStorageCache {
+
+    @Deprecated
     private HashMap<String, Vector<ScanBlockStruct>> cachedBlockList;
 
     public PlayerBlockStorageCache(){
@@ -20,5 +23,21 @@ public class PlayerBlockStorageCache {
 
     public Vector<ScanBlockStruct> getBlock(String name){
         return cachedBlockList.get(name);
+    }
+
+
+    private HashMap<String, PlayerBlockAreaInfoVO> playersBlockAreaInfo;
+
+    public boolean isOneTapped(String PID) {
+
+        PlayerBlockAreaInfoVO info = playersBlockAreaInfo.get(PID);
+
+        return info.firstBlock == null;
+    }
+
+    public void setFirstBlock(String PID, Block block) {
+        PlayerBlockAreaInfoVO info = playersBlockAreaInfo.get(PID);
+
+        info.lastBlock = block;
     }
 }
